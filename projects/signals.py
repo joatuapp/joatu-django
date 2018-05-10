04778a61b770fecb09338e3b0d7d188052ac734c
txWang <txwang.cs@gmail.com>
2018-05-10 01:16:14 -0400

    fix create project bug

--- projects/signals.py
+++ projects/signals.py
@@ -1,4 +1,5 @@
 from django.db.models.signals import post_save
+from django.db.models import ObjectDoesNotExist
 
 from .models import Project, ProjectStatus, ProjectIsValidated, ProjectHub
 from profiles.models import ProfileHub
@@ -18,13 +19,16 @@ def project_created_or_updated(sender, update_fields, **kwargs):
             instance.city
         )
         ProjectHub.objects.create(project=instance, lat=lat_cal, lng=lng_cal)
-        project_geo = ProjectHub.objects.get(project=instance)
-        hub_selected = ProfileHub.objects.get(profile=instance.organizer).hub
-        hub_geo = HubGeolocation.objects.get(hub=hub_selected)
-        distance = distance_calculation(project_geo, hub_geo)
-        project_geo.hub = hub_selected
-        project_geo.distance_km = distance
-        project_geo.save()
+        try:
+            project_geo = ProjectHub.objects.get(project=instance)
+            hub_selected = ProfileHub.objects.get(profile=instance.organizer).hub
+            hub_geo = HubGeolocation.objects.get(hub=hub_selected)
+            distance = distance_calculation(project_geo, hub_geo)
+            project_geo.hub = hub_selected
+            project_geo.distance_km = distance
+            project_geo.save()
+        except ObjectDoesNotExist:
+            pass
 
 
 post_save.connect(project_created_or_updated, sender=Project)
