de1abc3f00acfcae877ba42972f648bdbc717ddd
moileretour <thomas.corblin@gmail.com>
2018-05-14 13:16:09 -0400

    removed alert

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
     var userId = $('#userId').attr('value');
-    alert(userId);
     $.getJSON('/api/profiles/' + userId + '/', function (data) {
         $('#numb_CAPS').text(data.profile_wallet.wallet);
     });
