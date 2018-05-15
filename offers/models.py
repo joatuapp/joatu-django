8984d8644f311343f2e609426f3116ad625ccae9
Santosh Yadav <santosh.yadav198613@gmail.com>
2018-05-15 21:39:01 +0530

    added sample test case

--- offers/models.py
+++ offers/models.py
@@ -10,21 +10,19 @@ class Offer(models.Model):
     street = models.CharField(max_length=200, blank=False)
     postal_code = models.CharField(max_length=10, blank=False)
     city = models.CharField(max_length=50, blank=False)
-    seller=models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='profile_offers')
+    seller = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='profile_offers')
     is_CAPS = models.BooleanField(default=True)
     is_BARTER = models.BooleanField(default=True)
     is_GIVE = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     price_CAPS = models.PositiveIntegerField(null=True, blank=True)
-    price_barter = models.CharField(max_length = 200, null=True, blank=True)
+    price_barter = models.CharField(max_length=200, null=True, blank=True)
 
 
 class OfferHub(models.Model):
     offer = models.OneToOneField(Offer, on_delete=models.CASCADE)
-    hub = models.ForeignKey(Hub, on_delete=models.CASCADE, null= True, related_name='offers')
+    hub = models.ForeignKey(Hub, on_delete=models.CASCADE, null=True, related_name='offers')
     distance_km = models.DecimalField(max_digits=10, decimal_places=3, blank=False, null=True)
     lat = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
     lng = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
-
-
--- offers/tests.py
+++ offers/tests.py
@@ -1,3 +1,50 @@
 from django.test import TestCase
+from .models import Offer, Profile, OfferHub, Hub, ProfileHub
+from accounts.models import User
+
 
 # Create your tests here.
+
+class OfferTest(TestCase):
+    def setUp(self):
+        user = User.objects.create(
+            username='santosh.yadav198613',
+            email='santosh.yadav198613@gmail.com'
+        )
+
+        seller = Profile.objects.create(
+            user=user,
+            display_name='Test User',
+            birth_date='1986-11-13',
+            street='Pune',
+            postal_code='411027',
+            city='Pune',
+            country='India'
+        )
+        offer = Offer.objects.create(
+            title='Test', description='Test Offer', number='9220586270',
+            street='Pune', postal_code='411027', city='Pune', is_CAPS=True,
+            is_BARTER=True, is_GIVE=True, price_CAPS=10, price_barter='100',
+            seller=seller
+        )
+
+        hub = Hub.obejcts.create(
+            hub_name='Test Hub', number='9220586270',
+            street='Pune', postal_code='411027', city='Pune',
+            state='Pune', country='Inidia', description='Test Hub',
+            website='www.abc.com', email='testhub@test.com'
+        )
+
+        OfferHub.objects.create(
+            offer=offer, hub=hub, distance_km=10, lat=10.9, lng=11.9
+        )
+
+        ProfileHub.objects.create(
+            hub=hub,
+            profile=seller,
+            distance_km=10
+        )
+
+    def test_get_offer(self):
+        offer_name = Offer.objects.get(title='Test')
+        self.assertEqual(offer_name.description, "Test Offer")
