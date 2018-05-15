16fbb0cc416c73e3e09601a0a616a6445ecf3a19
moileretour <31390753+moileretour@users.noreply.github.com>
2018-05-15 17:02:54 -0400

    Fix /api/profiles// 404 (Not Found)

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
     var userId = $('#userId').attr('value');
-    $.getJSON('/api/profiles/' + userId + '/', function (data) {
-        $('#numb_CAPS').text(data.profile_wallet.wallet);
-    });
+    if (userId != '') {  //fix /api/profiles// 404 (Not Found) fix
+        $.getJSON('/api/profiles/' + userId + '/', function (data) {
+            $('#numb_CAPS').text(data.profile_wallet.wallet);
+        });
+    }
 });
