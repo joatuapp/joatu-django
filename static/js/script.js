4bb15e8021d1abcc894e369ae1f9d449e6e3ddb4
moileretour <31390753+moileretour@users.noreply.github.com>
2018-05-17 14:20:06 -0400

    fix bug api/profiles//

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     var userId = $('#userId').attr('value');
-    if (userId != '') {  //fix /api/profiles// 404 (Not Found) fix
+    if (userId) {  //fix /api/profiles// 404 (Not Found) fix
         $.getJSON('/api/profiles/' + userId + '/', function (data) {
             $('#numb_CAPS').text(data.profile_wallet.wallet);
         });
