17304714e6178bbc0184f303e505b27b3c8d01ba
ThomasCorblin <31390753+ThomasCorblin@users.noreply.github.com>
2018-07-28 19:31:39 -0400

    Revert "Issue05 sami"

--- joatu/settings/base.py
+++ joatu/settings/base.py
@@ -88,7 +88,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-   # 'joatu.middleware.ProfileRequiredMiddleware',
+    'joatu.middleware.ProfileRequiredMiddleware',
     'joatu.middleware.LoginRequiredMiddleware',
 ]
 
